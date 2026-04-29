e3d8ea1f12a20780b6212da1e11373a300b9be51
Farwa Zafar <pov.farwa@gmail.com>
2026-04-29 20:35:56 +0500

    write js code and make little bit improvements in the css

--- game.js
+++ game.js
@@ -20,7 +20,7 @@ let dino = {}
 
 function resetDino(){
     dino = {
-        x: 80;
+        x: 80,
         y: GROUND,
         w: 48,
         h: 46,
@@ -87,4 +87,78 @@ let popups = [];
 
 function spawnPopup(text, x, y){
     popups.push({ text, x, y, life: 40, maxLife: 40 })
+}
+
+function roundRect(ctx, x, y, w, h, r){
+    ctx.bginPath();
+    ctx.moveTo(x + r, y)
+    ctx.lineTo(x + w - r, y)
+    ctx.quadraicCurveto(x + w, x + W, y + r)
+    ctxlineTo(x + r, y + h)
+    ctx.quadraticCurveTo(x, y + H, x, y + h - r)
+    ctx.lineto(x, y + r)
+    ctx.cloasePath();
+}
+
+function drawDino(d){
+    ctx.save()
+
+    const cs = d.x + d.w / 2
+    const cy = d.y - d.h / 2
+    ctx.translate(cx, cy)
+    ctx.scale(d.squishX, d.squishY)
+    ctx.translate(-cx, -cy)
+    const X = d.x
+    const y = d.y - d.h
+    const clr = d.dead ? '#555' : '#fff';
+    ctx.fillStyle = 'rgba(255, 255, 255, 0.04'
+    ctx.ellipse(X + d.w / 2. Ground + 4, 20, 4, 0, 0, Math.PI * 2)
+    ctx.fill()
+
+}
+
+ctx.strokeStyle = clr; ctx.lineWidth = 3, ctx.fillStyle = '#000'
+ctx.beginPath()
+ctx.moveTo(X + 6, Y + 30)
+ctx.quatraticCurveTo(X - 16, Y + 38, X - 8, Y + 50)
+ctx.quadraticCurveto(X - 4, Y + 54, X + 2, Y + 48)
+ctx.quadraticCurveTo(X - 4, Y + 38, X + 8, Y + 34)
+ctx.closepath()
+ctx.fill()
+
+
+ctx.strokestyle = clr; ctx.lineWidth = 2; ctx.fillStyle = '#000'
+roundRect(ctx, X + 4, Y + 22, 32, 26, 6)
+ctx.fill(); ctx.stroke()
+roundRect(ctx, X + 6, Y + 12, 16, 14, 4)
+ctx.fill(); ctx.stroke()
+roundRect(ctx, X + 12, Y - 2, 30, 22, 8)
+ctx.fill(); ctx.stroke()
+roundRect(ctx, X + 30, Y + 8, 16, 10, 4)
+ctx.fill(); ctx.stroke()
+ctx.fillStyle = clr;
+ctx.fillRect(X + 42, Y + 10, 2, 2)
+ctx.fillStyle = clr
+roundRect(ctx, X + 18, Y + 2, 12, 11, 5)
+ctx.fill()
+
+if(!d.dead){
+    if(d.eyeOpen){
+        ctx.fillStyle = '#000'
+        ctx.beginPath(); ctx.arc(X + 25, Y + 8, 3.5, 0, Math.PI * 2); ctx.fill()
+        ctx.fillStyle = '#fff'
+        ctx.beginPath(); ctx.arc(X + 26.5, Y + 6.5, 1, 0, Math.PI * 2); ctx.fill();
+    }else{
+        ctx.stroke = '#000'; ctx.lineWidth = 2;
+        ctx.beginPath(); ctx.moveTo(X + 19, Y + 7); ctx.lineTo(X + 29, Y + 7); ctx.stroke()
+    }
+
+    ctx.fillStyle = clr;
+    ctx.fillRect(X + 20, Y + 1, 2, 2)
+    ctx.fillRect(X + 24, Y,     2, 2)
+    ctx.fillRect(X + 28, Y + 1, 2, 2);
+} else{
+    ctx.strokestyle = '#000'; ctx.lineWidth = 2
+    ctx.beginPath(); ctx.moveTo(X + 19, Y + 3); ctx.lineTo(X + 29, Y + 12); ctx.stroke()
+    ctx.beginPath(); ctx.moveTo(X + 29, Y + 3); ctx.lineTo(X + 19, Y + 12); ctx.stroke();
 }
